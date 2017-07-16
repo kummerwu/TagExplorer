d5a14566377cce690cfb4c3fe51707540eff9cd9
kummer wu <kummerwu@outlook.com>
2017-07-16 23:43:24 +0800

    将临时的进程名改为正式的名称

--- LuceneTest/Properties/Resources.Designer.cs
+++ LuceneTest/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace TagExplorer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LuceneTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TagExplorer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
