c33c99195fce3eed7165abb098c84fe7c9044496
kummer wu <kummerwu@outlook.com>
2017-11-05 12:52:08 +0800

    优化剪贴板操作异常的处理。

--- LuceneTest/Utils/Clip/ClipBoardSafe.cs
+++ LuceneTest/Utils/Clip/ClipBoardSafe.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;               // For Process class
 using System.Runtime.InteropServices;   // For DllImport's
 namespace TagExplorer.Utils
 {
+    //恶心的WPF剪贴板和Z，总是导致剪贴板操作失败
     class ClipBoardSafe
     {
         [DllImport("user32.dll")]
@@ -12,30 +13,65 @@ namespace TagExplorer.Utils
         [DllImport("user32.dll", SetLastError = true)]
         static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
 
+        static DateTime startTime;
         public static bool SetText(string txt)
         {
-            DateTime start = DateTime.Now;
-            for(int i = 0;i<20;i++)
+            startTime = DateTime.Now;
+            if (SetText(txt, 1)) return true;
+            else if (SetText(txt, 3)) return true;
+            else if (SetText(txt, 5)) return true;
+            else
+            {
+                ReportErr(txt);
+                return false;
+            }
+        }
+        private static bool SetText(string txt,int retryCnt)
+        {
+            if (SetTextWinFormClip(txt, retryCnt)) return true;
+            else if (SetTextWPFClip(txt, retryCnt)) return true;
+            else if (SetTextByData(txt, retryCnt)) return true;
+            else return false;
+        }
+        private delegate void SetClipTxt(string txt);
+        private static bool SetTxtTry(string txt,int retryCnt,SetClipTxt func)
+        {
+            if ((DateTime.Now - startTime).TotalSeconds > 5)
+            {
+                return false;
+            }
+            for (int i = 0; i < retryCnt; i++)
             {
                 try
                 {
-                    Clipboard.SetText(txt);
+                    func(txt);
                     return true;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                    if ((DateTime.Now - start).TotalSeconds > 5)
-                    {
-                        ReportErr(txt);
-                        return false;
-                    }
+                    Logger.E(e);
                     System.Threading.Thread.Sleep(10);
                 }
             }
-            ReportErr(txt);
+            
             return false;
         }
-        public static Process ProcessHoldingClipboard()
+        //使用window.form的剪贴板
+        private static bool SetTextWinFormClip(string txt, int retryCnt)
+        {
+            return SetTxtTry(txt, retryCnt, System.Windows.Forms.Clipboard.SetText);
+        }
+        //使用WPF的剪贴板
+        private static bool SetTextWPFClip(string txt, int retryCnt)
+        {
+            return SetTxtTry(txt, retryCnt, Clipboard.SetText);
+        }
+        //使用WPF的剪贴板 SetDataObject
+        private static bool SetTextByData(string txt,int retryCnt)
+        {
+            return SetTxtTry(txt, retryCnt, Clipboard.SetDataObject);
+        }
+        private static Process ProcessHoldingClipboard()
         {
             Process theProc = null;
 
@@ -64,7 +100,7 @@ namespace TagExplorer.Utils
 
             return theProc;
         }
-        public static void ReportErr(string txt)
+        private static void ReportErr(string txt)
         {
             Process p = ProcessHoldingClipboard();
             if (p == null)
@@ -73,7 +109,7 @@ namespace TagExplorer.Utils
             }
             else
             {
-                MessageBox.Show("将内容拷贝到剪贴板失败" +p.MainModule.FileName+"\r\n"+p.MainWindowTitle+"\r\n"+ txt, "设置剪贴板错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("将内容拷贝到剪贴板失败：\r\n占用剪贴板程序为：" +p.MainModule.FileName+"\r\n窗口标题为："+p.MainWindowTitle+"\r\n附-操作数据："+ txt, "设置剪贴板错误", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
